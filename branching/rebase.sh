0a7f26c3dfe1c16991bd73dd5a847e88ebafc65c
Aleksandr Zhukov <pukoff@gmail.xom>
2021-12-26 14:12:43 +0300

    Merge branch 'git-merge'
    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-# display command line options
-
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
