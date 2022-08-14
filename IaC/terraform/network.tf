8e9ee8b30608399b928338020b8a7e381ecd4405
Aleksandr Zhukov <PukOFF@gmail.com>
2022-08-14 15:09:47 +0300

    change .gitignore

--- IaC/terraform/network.tf
+++ IaC/terraform/network.tf
@@ -5,6 +5,6 @@ resource "yandex_vpc_network" "my_network" {
 resource "yandex_vpc_subnet" "my_subnet" {
   name = "my_subnet"
   v4_cidr_blocks = ["10.130.0.0/16"]
-  zone           = var.yc_zone
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_network.id
 }
--- IaC/terraform/vars.tf
+++ IaC/terraform/vars.tf
@@ -1,13 +1,13 @@
 variable "yc_token" {
-   default = ""
+   default = "AQAAAAAITr-AAATuwQGvx2-xd00CgyUxcO-fhik"
 }
 
 variable "yc_cloud_id" {
-  default = ""
+  default = "b1ggfq7sd82lqcg3psgp"
 }
 
 variable "yc_folder_id" {
-  default = ""
+  default = "b1g4inuvv0jlp45umhpf"
 }
 
 variable "yc_zone" {
