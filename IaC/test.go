a2e77b5ee4f3c1a9e038a6e66f336aeeb2a0f2f8
Aleksandr Zhukov <PukOFF@gmail.com>
2022-08-22 21:28:29 +0300

    05-golang

---
+++ IaC/test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
+	fmt.Println("Massive: ", x)
+	min := x[0]
+
+	/*        var input float64
+	          fmt.Scanf("%f", &input)
+
+	          output := input * 0.3048
+
+	          fmt.Println("Number of foots: ", output)
+	*/
+}
